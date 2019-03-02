31ce5d38fb4879ebb583d8d58697c06efda19a05
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-03-02 22:30:01 +0100

    Add neccessary

--- packages/Auxilium-App/android/app/src/main/java/com/auxilium/MainApplication.java
+++ packages/Auxilium-App/android/app/src/main/java/com/auxilium/MainApplication.java
@@ -3,6 +3,7 @@ package com.auxilium;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.sentry.RNSentryPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new RNSentryPackage(),

6d4299cbada41ba4dd3ea49dd9c527aeae23bd7e
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-03-07 09:38:50 +0100

    Add missing type

--- packages/Auxilium-Server/src/app.ts
+++ packages/Auxilium-Server/src/app.ts
@@ -13,12 +13,12 @@ const io = socketIO.listen(server);
 
 Sentry.init({ dsn: env.SENTRY_DSN });
 
-app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
 app.set('io', io);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
-app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
+app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
 
 app.listen(env.PORT, () => console.log(`Server started on port ${env.PORT}`));
 

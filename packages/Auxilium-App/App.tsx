ec11de71b6f4c331b860b534173e96676240348a
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-03-07 09:10:04 +0100

    Add Sentry

--- packages/Auxilium-App/App.tsx
+++ packages/Auxilium-App/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import Sentry from 'sentry-expo';
 import { useScreens } from 'react-native-screens';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { store, persistor } from './store';
 import Root from './src/Root';
 
 persistor.purge();
+Sentry.config(
+	'https://018ed01c2b844dc1bab9fa5a84517b24@sentry.io/1409956'
+).install();
 useScreens();
 
 const App = () => (

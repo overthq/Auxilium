c50e7aa2d5b5e7d96b1e0e09a17054eb54157c04
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-03-07 09:36:50 +0100

    Sentry Server setup

--- packages/Auxilium-Server/src/app.ts
+++ packages/Auxilium-Server/src/app.ts
@@ -1,20 +1,25 @@
 import * as http from 'http';
 import * as express from 'express';
 import * as socketIO from 'socket.io';
+import * as Sentry from '@sentry/node';
 import router from './router';
+import env from './config/env';
 import './config/database';
 
 const app: express.Application = express();
-const port: number | any = process.env.PORT || 4000;
 
 const server = http.createServer(app);
 const io = socketIO.listen(server);
 
+Sentry.init({ dsn: env.SENTRY_DSN });
+
+app.use(Sentry.Handlers.requestHandler());
 app.set('io', io);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
+app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(env.PORT, () => console.log(`Server started on port ${env.PORT}`));
 
 export default app;

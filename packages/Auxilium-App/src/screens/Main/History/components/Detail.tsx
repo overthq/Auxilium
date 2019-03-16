2fa500ac8230a5545406681afb1cff30e535b860
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-03-16 11:50:47 +0100

    Details page

--- packages/Auxilium-App/src/screens/Main/History/components/Detail.tsx
+++ packages/Auxilium-App/src/screens/Main/History/components/Detail.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+	container: {
+		height: 50,
+		width: 0.8 * width,
+		borderRadius: 4,
+		backgroundColor: '#D3D3D3'
+	},
+	text: {
+		fontFamily: 'Muli Regular',
+		fontSize: 16
+	}
+});
 
 const Detail = ({ location: { coordinates } }: Emergency) => (
-	<View>
-		<Text>At {JSON.stringify(coordinates)}</Text>
+	<View style={styles.container}>
+		<Text style={styles.text}>Longitude: {`${coordinates[0]}`}</Text>
+		<Text style={styles.text}>Latitude: {`${coordinates[1]}`}</Text>
 	</View>
 );
 

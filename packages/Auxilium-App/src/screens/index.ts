cd1057248d8c3cae2eed6347290ee9d31b770884
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-04-03 02:23:38 +0100

    Remove old Main from Navigator

--- packages/Auxilium-App/src/screens/index.ts
+++ packages/Auxilium-App/src/screens/index.ts
@@ -1,11 +1,11 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import Onboarding from './Onboarding';
-import Main from './Main';
 import NewThoughts from './NewThoughts';
+// import Main from './Main';
 
 export default createAppContainer(
 	createStackNavigator(
-		{ Onboarding, NewThoughts, Main },
+		{ Onboarding, NewThoughts },
 		{
 			headerMode: 'none',
 			defaultNavigationOptions: {

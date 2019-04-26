29b2f32134cb54d07fc04ebb66a784732179bd3f
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-04-26 22:29:08 +0100

    Fix linting issues

--- packages/Auxilium-App/env.ts
+++ packages/Auxilium-App/env.ts
@@ -2,10 +2,12 @@ import { Constants } from 'expo';
 
 const ENV = {
 	dev: {
-		apiUrl: __DEV__ ? `http://${Constants.manifest.debuggerHost
-			.split(`:`)
-			.shift()
-			.concat(`:4000/`)}` : ''
+		apiUrl: __DEV__
+			? `http://${Constants.manifest.debuggerHost
+					.split(`:`)
+					.shift()
+					.concat(`:4000/`)}`
+			: ''
 	},
 	staging: {
 		apiUrl: 'https://auxilium-staging.herokuapp.com/'

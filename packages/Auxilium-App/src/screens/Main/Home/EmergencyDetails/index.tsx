285825be9a12b30ca45d9182ebc0c73e41476c8d
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-04-26 21:27:24 +0100

    Added icon margin

--- packages/Auxilium-App/src/screens/Main/Home/EmergencyDetails/index.tsx
+++ packages/Auxilium-App/src/screens/Main/Home/EmergencyDetails/index.tsx
@@ -89,7 +89,12 @@ class EmergencyDetails extends React.Component<
 				</MapView>
 				<View style={styles.descriptionView}>
 					<View style={{ flexDirection: 'row' }}>
-						<Feather name='navigation' color='#D3D3D3' size={16} />
+						<Feather
+							name='navigation'
+							color='#D3D3D3'
+							size={16}
+							style={{ marginRight: 10 }}
+						/>
 						<Text style={styles.description}>
 							{`${roundNum(latitude)}, ${roundNum(longitude)}`}
 						</Text>

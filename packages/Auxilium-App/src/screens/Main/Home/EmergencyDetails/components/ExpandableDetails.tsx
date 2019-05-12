eba91de2b116b5eb9e77c38ab2f052f998667614
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-05-12 21:37:29 +0100

    Attempt at using Animated.event

--- packages/Auxilium-App/src/screens/Main/Home/EmergencyDetails/components/ExpandableDetails.tsx
+++ packages/Auxilium-App/src/screens/Main/Home/EmergencyDetails/components/ExpandableDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet, Text } from 'react-native';
 import Animated from 'react-native-reanimated';
 import {
 	PanGestureHandler,
@@ -8,22 +8,46 @@ import {
 
 const { height } = Dimensions.get('window');
 
-const ExpandableDetails = () => {
+interface IExpandableDetailsProps {
+	description?: string;
+	longitude: number;
+	latitude: number;
+}
+
+const ExpandableDetails = ({
+	description,
+	longitude,
+	latitude
+}: IExpandableDetailsProps) => {
 	const animatedHeight = new Animated.Value(height / 4);
 	const onPanGestureEvent = (event: PanGestureHandlerGestureEvent) => {
-		Animated.interpolate(animatedHeight, {
-			inputRange: [height / 4, height / 2],
-			outputRange: [0, 1],
-			extrapolate: Animated.Extrapolate.CLAMP
-		});
+		console.log(event.nativeEvent.y);
+		Animated.event(
+			[
+				{
+					nativeEvent: {
+						contentOffset: {
+							y: event.nativeEvent.y
+						}
+					}
+				}
+			],
+			{ useNativeDriver: true }
+		);
+		// Use Animated.event to animate height between height / 4 and height / 2
+		// Or find out if translationY is a good idea.
+		// Or, we could use scaleY to increase the vertical height
+		// Go from scaleY: height / 4 to height / 2
 	};
 
 	return (
-		<PanGestureHandler
-			onHandlerStateChange={onPanGestureEvent}
-			onGestureEvent={onPanGestureEvent}
-		>
-			<Animated.View style={[styles.container, { height: animatedHeight }]} />
+		<PanGestureHandler onGestureEvent={onPanGestureEvent}>
+			<Animated.View style={[styles.container, { height: animatedHeight }]}>
+				<Text>{description}</Text>
+				<Text>{`${longitude}, ${latitude}`}</Text>
+				{/* Maybe the name of the city, country or address. */}
+				{/* We can also use the animation value to interpolate the opacity of the name of place text */}
+			</Animated.View>
 		</PanGestureHandler>
 	);
 };
@@ -32,7 +56,8 @@ const styles = StyleSheet.create({
 	container: {
 		backgroundColor: '#505050',
 		borderTopLeftRadius: 10,
-		borderTopRightRadius: 10
+		borderTopRightRadius: 10,
+		padding: 20
 	}
 });
 

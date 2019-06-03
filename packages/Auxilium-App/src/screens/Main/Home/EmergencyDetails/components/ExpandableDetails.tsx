045d1533d7214cacdab6f50562a6d0b669f1c710
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-06-03 02:56:19 +0100

    Start styling and add place state to EmergencyDetails

--- packages/Auxilium-App/src/screens/Main/Home/EmergencyDetails/components/ExpandableDetails.tsx
+++ packages/Auxilium-App/src/screens/Main/Home/EmergencyDetails/components/ExpandableDetails.tsx
@@ -1,61 +1,76 @@
 import React from 'react';
 import { Dimensions, StyleSheet, Text } from 'react-native';
 import Animated from 'react-native-reanimated';
-import {
-	PanGestureHandler,
-	PanGestureHandlerGestureEvent
-} from 'react-native-gesture-handler';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 import { LocationHelpers } from '../../../../../helpers';
 
 const { height } = Dimensions.get('window');
+
+interface IExpandableDetailsState {
+	place: string;
+}
 
 interface IExpandableDetailsProps {
 	description?: string;
 	longitude: number;
 	latitude: number;
 }
-
-const ExpandableDetails = ({
-	description,
-	longitude,
-	latitude
-}: IExpandableDetailsProps) => {
-	const animatedHeight = new Animated.Value(height / 4);
-	const onPanGestureEvent = (event: PanGestureHandlerGestureEvent) => {
-		console.log(event);
-		Animated.event(
-			[
-				{
-					nativeEvent: {
-						contentOffset: { y: animatedHeight }
-					}
-				}
-			],
-			{ useNativeDriver: true }
-		);
-		// Use Animated.event to animate height between (height / 4) and (height / 2)
-		// Or find out if translationY is a good idea.
-		// Or, we could use scaleY to increase the vertical height
-		// Go from scaleY: (height / 4) to (height / 2)
+class ExpandableDetails extends React.PureComponent<
+	IExpandableDetailsProps,
+	IExpandableDetailsState
+> {
+	state: IExpandableDetailsState = {
+		place: ''
 	};
 
-	// Use the animated value
-	return (
-		<PanGestureHandler onGestureEvent={onPanGestureEvent}>
-			<Animated.View style={[styles.container, { height: animatedHeight }]}>
-				<Text style={styles.emergencyDescription}>{description}</Text>
-				<Text style={styles.emergencyAddress}>
-					{LocationHelpers.getAddressFromCoords({ longitude, latitude })}
-				</Text>
-				<Text style={styles.emergencyLongLat}>
-					{`${longitude}, ${latitude}`}
-				</Text>
-				{/* Maybe the name of the city, country or address. */}
-				{/* We can also use the animation value to interpolate the opacity of the name of place text */}
-			</Animated.View>
-		</PanGestureHandler>
-	);
-};
+	componentDidMount() {
+		this.loadPlace();
+	}
+
+	loadPlace = async () => {
+		const { longitude, latitude } = this.props;
+		const place = await LocationHelpers.getAddressFromCoords({
+			longitude,
+			latitude
+		});
+		this.setState({ place });
+	};
+
+	render() {
+		const animatedHeight = new Animated.Value(height / 4);
+		const onPanGestureEvent = () => {
+			Animated.event(
+				[
+					{
+						nativeEvent: {
+							contentOffset: { y: animatedHeight }
+						}
+					}
+				],
+				{ useNativeDriver: true }
+			);
+			// Use Animated.event to animate height between (height / 4) and (height / 2)
+			// Or find out if translationY is a good idea.
+			// Or, we could use scaleY to increase the vertical height
+			// Go from scaleY: (height / 4) to (height / 2)
+		};
+
+		const { description, longitude, latitude } = this.props;
+		const { place } = this.state;
+		return (
+			<PanGestureHandler onGestureEvent={onPanGestureEvent}>
+				<Animated.View style={[styles.container, { height: animatedHeight }]}>
+					<Text style={styles.emergencyDescription}>{description}</Text>
+					<Text style={styles.emergencyAddress}>{place}</Text>
+					<Text style={styles.emergencyLongLat}>
+						{`${longitude}, ${latitude}`}
+					</Text>
+					{/* We can also use the animation value to interpolate the opacity of the name of place text */}
+				</Animated.View>
+			</PanGestureHandler>
+		);
+	}
+}
 
 const styles = StyleSheet.create({
 	container: {

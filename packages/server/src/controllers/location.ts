c8bae3eb1ec69d370b5f9d639bc796914526c328
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-06-14 13:22:53 +0100

    Move API_URL into constant

--- packages/server/src/controllers/location.ts
+++ packages/server/src/controllers/location.ts
@@ -2,13 +2,15 @@ import fetch from 'node-fetch';
 import env from '../config/env';
 import { RequestHandler } from 'express';
 
+const MAPBOX_BASE_URL = 'https://api.mapbox.com/directions/v5';
+
 export const getRoute: RequestHandler = async (req, res) => {
 	const { from, to } = req.query;
 	const [fromLongitude, fromLatitude] = from.split(',');
 	const [toLongitude, toLatitude] = to.split(',');
 	try {
 		const response = await fetch(
-			`https://api.mapbox.com/directions/v5/mapbox/walking/${fromLongitude},${fromLatitude};${toLongitude},${toLatitude}.json?access_token=${env.MAPBOX_ACCESS_TOKEN}&geometries=geojson`
+			`${MAPBOX_BASE_URL}/mapbox/walking/${fromLongitude},${fromLatitude};${toLongitude},${toLatitude}.json?access_token=${env.MAPBOX_ACCESS_TOKEN}&geometries=geojson`
 		);
 		const { routes } = await response.json();
 		const { coordinates } = routes[0].geometry;
@@ -34,7 +36,7 @@ export const getAddress: RequestHandler = async (req, res) => {
 	const { longitude, latitude } = req.query;
 	try {
 		const response = await fetch(
-			`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${env.MAPBOX_ACCESS_TOKEN}`
+			`${MAPBOX_BASE_URL}/mapbox.places/${longitude},${latitude}.json?access_token=${env.MAPBOX_ACCESS_TOKEN}`
 		);
 		const { features } = await response.json();
 		return res.status(200).json({

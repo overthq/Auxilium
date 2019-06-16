6124495f5022157e220699af9fffcae0f99151d9
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-06-16 17:48:53 +0100

    Add development clause

--- packages/app/env.ts
+++ packages/app/env.ts
@@ -2,11 +2,12 @@ import { Constants } from 'expo';
 
 const ENV = {
 	dev: {
-		apiUrl:
-			`http://${Constants.manifest.debuggerHost
-				.split(`:`)
-				.shift()
-				.concat(`:4000/`) || ''}` || ''
+		apiUrl: __DEV__
+			? `http://${Constants.manifest.debuggerHost
+					.split(`:`)
+					.shift()
+					.concat(`:4000/`) || ''}`
+			: ''
 	},
 	staging: {
 		apiUrl: 'https://auxilium-staging.herokuapp.com/'

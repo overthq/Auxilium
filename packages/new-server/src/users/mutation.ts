01643c0e25b277cd2caf6082a2cb38d79981ccb0
Oluwakorede Fashokun <koredefashokun@gmail.com>
2019-11-06 02:22:19 +0100

    Add resolver definition for user functions

--- packages/new-server/src/users/mutation.ts
+++ packages/new-server/src/users/mutation.ts
@@ -1,3 +1,22 @@
-const userMutation = {};
+import { User } from '../models';
+
+const userMutation = {
+	authenticate: async (_, { input }) => {
+		const { deviceId, pushToken } = input;
+		if (!deviceId || !pushToken) {
+			throw new Error('Please use this from a device ;)');
+		}
+		try {
+			let user = await User.findOne({ deviceId });
+			if (!user) {
+				const newUser = await User.create({ deviceId, pushToken });
+				user = newUser;
+			}
+			return user;
+		} catch (error) {
+			throw error;
+		}
+	}
+};
 
 export default userMutation;
--- packages/new-server/src/users/types.ts
+++ packages/new-server/src/users/types.ts
@@ -2,10 +2,16 @@ const User = `
 	type User {
 		_id: String!
 		deviceId: String!
+		pushToken: String!
+	}
+
+	input AuthInput {
+		deviceId: String
+		pushToken: String
 	}
 
 	extend type Mutation {
-
+		authenticate(input: AuthInput!): User!
 	}
 `;
 

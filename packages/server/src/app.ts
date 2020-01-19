9a140f6520a32e3671cd571dd8f1f6a2bcd0c8ac
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-01-19 20:58:58 +0100

    Add Redis port and host to env files and remove dependence on env.ts file

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import * as Sentry from '@sentry/node';
-
 import router from './routes';
-import env from './config/env';
 import './config/database';
 
 const app: express.Application = express();
-Sentry.init({ dsn: env.SENTRY_DSN });
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 app.use(Sentry.Handlers.requestHandler());
 app.use(express.json());
@@ -14,8 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 app.use(Sentry.Handlers.errorHandler());
 
-app.listen(env.PORT, () => {
-	console.log(`Server started on port ${env.PORT}`);
+const { PORT } = process.env;
+
+app.listen(Number(PORT), () => {
+	console.log(`Server started on port ${PORT}`);
 });
 
 export default app;
--- packages/server/src/config/redis.ts
+++ packages/server/src/config/redis.ts
@@ -1,6 +1,7 @@
 import redis from 'redis';
 
-const client = redis.createClient();
+const { REDIS_PORT, REDIS_HOST } = process.env;
+const client = redis.createClient(Number(REDIS_PORT), REDIS_HOST);
 
 client.on('error', error => {
 	throw new Error(error);

ecd84760f4efcaffbcada0b26b1ddb638a47e052
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-01-20 01:50:27 +0100

    Remove unused dashboard code

--- packages/dashboard/src/api/Emergencies.ts
+++
@@ -1,9 +0,0 @@
-export const getEmergencies = async ({ longitude, latitude }: Coordinates) => {
-	try {
-		const response = await fetch(`${process.env.API_URL}/emergencies/get`);
-		const { emergencies } = await response.json();
-		return emergencies;
-	} catch (error) {
-		alert(error.message);
-	}
-};

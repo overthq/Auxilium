feec30439d2d933da1c990a4cbca1ec741550f18
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-03-06 00:16:11 +0100

    Add placeholder text to the contacts overlay

--- packages/app/src/components/overlays/ContactsOverlay.tsx
+++ packages/app/src/components/overlays/ContactsOverlay.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Text, StyleSheet } from 'react-native';
 import { OverlaySlide } from '../Overlay';
+import { useAppSelector } from '../../../store';
 
 const ContactsOverlay = () => {
-	return <OverlaySlide title='Contacts'></OverlaySlide>;
+	const theme = useAppSelector(({ theme }) => theme);
+
+	return (
+		<OverlaySlide title='Contacts'>
+			<Text style={[styles.text, { color: theme.secondaryColor }]}>
+				This feature is not in development yet. It involves saving close
+				contacts to SQLite, so we can send them a text message whenever users
+				are in an emergency. Feel free to open a PR to implement this.
+			</Text>
+		</OverlaySlide>
+	);
 };
+
+const styles = StyleSheet.create({
+	text: {
+		fontSize: 16
+	}
+});
 
 export default ContactsOverlay;

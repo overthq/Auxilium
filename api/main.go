2c53ca6a8ba83d17d52df8ca438c1f35c7cbba9d
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-10-31 03:36:39 -0600

    Add empty route

--- api/main.go
+++ api/main.go
@@ -11,6 +11,10 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Do thing")
 }
 
+func createEmergency(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func fetchNearbyEmergencies(w http.ResponseWriter, r *http.Request) {
 
 }
@@ -18,11 +22,13 @@ func fetchNearbyEmergencies(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
     http.HandleFunc("/", homePage)
     http.HandleFunc("/emergencies", fetchNearbyEmergencies)
+    http.HandleFunc("/emergencies/new", createEmergency)
     log.Fatal(http.ListenAndServe(":4000", nil))
+
+    fmt.Println("Server running on port 4000")
 }
 
 func main() {
-	fmt.Println("Do something")
 	handleRequests()
 }
 
